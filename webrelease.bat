3258bbb9f4137e8c1a75b08246b790b7298cc110
octopuscart <octopuscartltd@gmail.com>
2024-10-13 22:46:31 +0530

    update files

--- webrelease.bat
+++ webrelease.bat
@@ -8,17 +8,13 @@ set minute=%TIME:~3,2%
 set second=%TIME:~6,2%
 set date=%year%-%month%-%day% %hour%:%minute%:%second%
 
-
 call flutter clean
 call flutter pub get
 del pubspec.lock
 
 call flutter build web --web-renderer html --release
 
-
-call firebase deploy --project zuriel-app
-
-
+call firebase deploy --project zuriel-5ef8b --only "hosting"
 
 call git add .
 call git commit -m "update website %date%"
